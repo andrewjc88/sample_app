735ce5b40db5523f7508c76f3dfb961c4f2c261c
andrewjc88 <andrewjc88@gmail.com>
2014-12-27 13:10:50 -0800

    Finish log in/log out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,30 +2,89 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+    #Define setup function
     def setup
+        #Use test user as setup function data
         @user = users(:tom)
+        #End defining setup function
     end
 
+    #Test login with invalid information so 
     test "login with invalid information" do
+        # Show login form
         get login_path
+        # Creating new session
         assert_template 'sessions/new'
+        # Input invalid user data
         post login_path, session: { email: "", password: "" }
+        # Starts new session  
         assert_template 'sessions/new'
+        # Show error flash that login creds were bad
         assert_not flash.empty?
+        # Creates new session
         get root_path
+        # Make sure flash doesn't follow to dashboard
         assert flash.empty?
     end
-    
+# Hypothesis: When a user logs in, They will be redireced to the profile page,
+#      The template shown will be the user profile template, the login path is
+#      not visible the log out path is visible, and the user path is for
+#      the current user.
 
-    test "login with valid information" do
-        get login_path
-        post login_path, session: { email: @user.email, password: 'password' }
-        assert_redirected_to @user
-        follow_redirect!
-        assert_template 'users/show'
+# Second hypothesis: When a gooed in user logs out, they will no longer be logged in,
+#        They will be redirected to the root url, the login link will be visible but
+#        the log out will not be visible, and the path will not be the current user's
+#        profile path. 
+
+    test "login with valid followed by the logout" do 
+        # Loads login page 
+        get login_path 
+        # Input correct user data
+        post login_path, session: { email: @user.email, password: 'password' } 
+        # Make sure 
+        assert is_logged_in?
+        # Redirecting to user dashboard.
+        assert_redirected_to @user 
+        # Visit rediret page.
+        follow_redirect! 
+        # Input user data into user dashboard template
+        assert_template 'users/show' 
+        # Removes login link
         assert_select "a[href=?]", login_path, count: 0
+        # Makes login link show
         assert_select "a[href=?]", logout_path
+        # Shows user profile links. 
         assert_select "a[href=?]", user_path(@user)
-
+        # 
+        delete logout_path
+        # 
+        assert_not is_logged_in?
+        # 
+        assert_redirected_to root_url
+        # Simulate a suer clicking logout in a second window.
+        delete logout_path
+        # 
+        follow_redirect!
+        #
+        assert_select "a[href=?]", login_path
+        # 
+        assert_select "a[href=?]", logout_path, count:0
+        # 
+        assert_select "a[href=?]", user_path(@user), count:0
     end
+
+    test "login with remembering" do
+        log_in_as(@user, remember_me: '1')
+        assert_not_nil cookies['remember_token']
+    end
+
+    test "login without remembering" do
+        log_in_as(@user, remember_me: '0')
+        assert_nil cookies['remember_token']
+    end
+
 end
+
+
+
+ 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,29 +1,41 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-    
+    #Validates login works with valid data input
     test "valid signup information" do
+        #load sign up page 
         get signup_path
+        # adding one to user.count object
         assert_difference 'User.count', 1 do
+            #Submits data to user database 
             post_via_redirect users_path, user: { 
-                name:                   "Example User", 
+                # Subits name field to database
+                name:                   "Example User",
+                # Subits email field to database 
                 email:                  "user@example.com", 
+                # Submits password data to database
                 password:               "password", 
+                # Submits password confirmation to database
                 password_confirmation:  "password" 
             }
  
         end
+        # Loads user dashboard.
         assert_template 'users/show'
+        # Checks if user is logged in. 
         assert is_logged_in?
     end
     
-   
-
+    # Tests to make sure invalid data isn't being accepted into database.
     test "invalid signup information" do
-      get signup_path
+        # Load the signup page
+        get signup_path
+        # Make sure that the user count doesnt change
       assert_no_difference 'User.count' do
+          #submit invalid user details to the signup path
           post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}
       end
+      #make sure the sign up page gets reloaded
       assert_template 'users/new'
   end
 end
